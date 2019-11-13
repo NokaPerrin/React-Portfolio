f1df5e9982f04424dcbae66764063a348e9a93cc
CameronP <nokaperrin@gmail.com>
2019-11-13 09:47:58 -0800

    Added Reactstrap and Bootstrap

--- react-portfolio/src/Navbar/index.js
+++ react-portfolio/src/Navbar/index.js
@@ -1,32 +1,86 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
 } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
 
-class Navbar extends Component {
+
+
+class NavbarClass extends Component {
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    closeNavbar() {
+        this.setState({
+            isOpen: false
+        });
+    }
+
+    handleClickOutside(event) {
+        const t = event.target;
+        if (this.state.isOpen && !t.classList.contains('navbar-toggler')) {
+            this.closeNavbar();
+        }
+    }
+
     render() {
+
         return (
             <Router>
                 <div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/users">Users</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                    <Navbar color="light" light expand="md">
+                        <NavbarBrand href="/">Cameron Perrin</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="/components/">Components</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                                </NavItem>
+                                <UncontrolledDropdown nav inNavbar>
+                                    <DropdownToggle nav caret>
+                                        Options
+                        </DropdownToggle>
+                                    <DropdownMenu right>
+                                        <DropdownItem>
+                                            Option 1
+                </DropdownItem>
+                                        <DropdownItem>
+                                            Option 2
+                </DropdownItem>
+                                        <DropdownItem divider />
+                                        <DropdownItem>
+                                            Reset
+                </DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
                 </div>
             </Router>
         )
     }
 }
-export default Navbar;
+export default NavbarClass;
