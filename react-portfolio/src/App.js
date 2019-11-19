709f1957b0d674aa3cc543893af1d0585b31c20b
CameronP <nokaperrin@gmail.com>
2019-11-19 08:52:38 -0800

    Update App.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Navbar';
-import Slider from './Components/Slider';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
         <Navbar />
-        <Slider />
-      </div>
     )
   }
 }
